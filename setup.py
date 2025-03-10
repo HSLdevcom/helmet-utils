27255c99274f5c408ec187deac8f52ee49f668d5
Santeri Hiitola <santeri.hiitola@outlook.com>
2025-03-10 10:31:37 +0200

    bump version number

--- setup.py
+++ setup.py
@@ -2,13 +2,13 @@ from setuptools import setup, find_packages
 
 setup(
     name='helmet_utils',
-    version='0.3',
+    version='0.4',
     description='Utilities for the processing and adjusting of data used by Helmet',
     author='Santeri Hiitola | HSL',
     license='EUPL',
     packages=find_packages(),
     package_data={
-        'helmet_utils': ['data/elevation_fixes.csv'],
+        'helmet_utils': ['data/elevation_fixes.csv', 'data/landcover.tif', 'data/SIJ2023_aluejako.gpkg'],
     },
     install_requires=[
         'geopandas',
